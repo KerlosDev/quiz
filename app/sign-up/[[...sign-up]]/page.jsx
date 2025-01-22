14ce6666bd11098976f8e44d3749b35b2977cff4
kerloshany3 <kerlostablet@gmail.com>
2025-01-22 03:12:33 +0200

    ds

--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -38,7 +38,7 @@ const page = () => {
                                 </a>
 
                                 <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-                                    Welcome to Ahmed El Sayed Platform 🧬
+                                    Welcome to Quizatk Platform 🧬
                                 </h2>
 
                                 <p className="mt-4 leading-relaxed text-white/90">
