50f61105b359c9b01b2bdbc6221abcb25afca781
kerloshany3 <kerlostablet@gmail.com>
2025-02-12 10:40:51 +0200

    enw

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ import Subjects from "./components/Subjects";
 export default function Home() {
 
   <Head>
-    <title>كويزاتك - أفضل منصة للاختبارات</title>
+    <title>كويزاتك - أفضل منصة لاختبارات الثانوية العامة</title>
     <meta name="description" content="منصة كويزاتك لاختبارات الثانوية العامه ." />
     <meta name="keywords" content="كويزاتك, منصة كويزاتك, quizatk,  اختبارات ثانوية عامة ,  مسابقات ثانوية عامة , موقع كويزاتك" />
     <meta property="og:title" content="كويزاتك - أفضل منصة لاختبارات الثانوية العامة" />
